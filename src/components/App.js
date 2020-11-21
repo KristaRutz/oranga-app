fc80703bbd84b1425851f9bc026b9307927eb7af
Krista Rutz <rutzkri000@gmail.com>
2020-11-21 11:22:23 -0800

    Add withFirebaseAuth package, include sign in/out button, and update firebase import statements for version 8.0

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,30 @@ import logo from "../assets/images/logo.svg";
 import "./App.css";
 import Footer from "./Footer";
 
-function App() {
+import withFirebaseAuth from "react-with-firebase-auth";
+import "firebase/auth";
+import firebase from "../firebase";
+
+const firebaseAppAuth = firebase.auth();
+const providers = {
+  emailProvider: new firebase.auth.EmailAuthProvider(),
+  googleProvider: new firebase.auth.GoogleAuthProvider(),
+};
+
+function App({ user, signOut, signInWithEmail, signInWithGoogle }) {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        {user ? <p>Hello, {user.displayName}</p> : <p>Please sign in.</p>}
+        {user ? (
+          <button onClick={signOut}>Sign out</button>
+        ) : (
+          <>
+            <button onClick={signInWithEmail}>Sign in with email</button>
+            <button onClick={signInWithGoogle}>Sign in with Google</button>
+          </>
+        )}
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -24,4 +43,8 @@ function App() {
   );
 }
 
-export default App;
+//export default App;
+export default withFirebaseAuth({
+  providers,
+  firebaseAppAuth,
+})(App);
