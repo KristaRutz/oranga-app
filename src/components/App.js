ff6b5f236e937d7e54ca04114a918097efd53bd6
Krista Rutz <rutzkri000@gmail.com>
2020-11-24 22:46:41 -0800

    Create an onboarding component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import SignIn from "./SignIn";
+import Onboarding from "./Onboarding";
 import Footer from "./Footer";
 
 function App() {
   return (
     <div className="App">
+      <Onboarding />
       <header className="App-header">
         <h1>Project Oranga</h1>
         <SignIn />
---
+++ src/components/Onboarding.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+function Onboarding() {
+  let userInputs = {
+    question1: 1,
+    question2: [],
+    question3: [],
+  };
+
+  return (
+    <div>
+      <h1>Onboarding</h1>
+      {
+        //Reflection question pane
+        // descriptive
+        // options to select
+        // info that comes up
+        // selection confirmation button
+      }
+    </div>
+  );
+}
+
+export default Onboarding;
