c6af538def323146698374a01d89978a0ce23361
Krista Rutz <rutzkri000@gmail.com>
2020-12-08 09:34:07 -0800

    Column changes

--- src/components/OnboardingFinish.js
+++ src/components/OnboardingFinish.js
@@ -1,21 +1,42 @@
 import React from "react";
+import styled from "styled-components";
 import SignIn from "./SignIn";
+
+const Layout = styled.div`
+  height: 80vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+const Row = styled.div`
+  display: flex;
+  align-items: flex-start;
+  justify-content: space-around;
+`;
+const Col = styled.div`
+  width: 50%;
+`;
+const CenteredCol = styled(Col)`
+  text-align: center;
+`;
 
 function OnboardingFinish({ handleBackClick }) {
   return (
-    <div>
-      <div>
-        <h2>You've already made progress in your mental wellness journey!</h2>
-        <p className="lead">
-          Within seconds, you will gain insights into your current wellbeing and
-          recieve personalized options for dealing with burnout.
-        </p>
-        <button onClick={handleBackClick}>Back</button>
-      </div>
-      <div>
-        <SignIn />
-      </div>
-    </div>
+    <Layout>
+      <Row>
+        <Col>
+          <h2>You've already made progress in your mental wellness journey!</h2>
+          <p className="lead">
+            Within seconds, you will gain insights into your current wellbeing
+            and recieve personalized options for dealing with burnout.
+          </p>
+          <button onClick={handleBackClick}>Back</button>
+        </Col>
+        <CenteredCol>
+          <SignIn />
+        </CenteredCol>
+      </Row>
+    </Layout>
   );
 }
 
