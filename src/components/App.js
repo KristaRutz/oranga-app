0d34853d380736f6593ebced93bfd4d82ce4d766
Krista Rutz <rutzkri000@gmail.com>
2020-12-09 08:55:27 -0800

    Update main screen: display onboarding if no user

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,17 @@
+import React, { useContext } from "react";
 import "./App.css";
+import { UserContext } from "../providers/UserProvider";
+
 import SignIn from "./SignIn";
 import OnboardingController from "./OnboardingController";
 import Footer from "./Footer";
 
 function App() {
+  const user = useContext(UserContext);
   return (
     <div className="App">
-      <OnboardingController />
+      <SignIn></SignIn>
+      {!user && <OnboardingController />}
       <Footer></Footer>
     </div>
   );
