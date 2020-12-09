c8d5c4e9285d1f13b87b87ebdae1b23594049da8
Krista Rutz <rutzkri000@gmail.com>
2020-12-09 09:14:05 -0800

    Implement the dashboard when user is logged in

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,19 @@
 import React, { useContext } from "react";
-import "./App.css";
 import { UserContext } from "../providers/UserProvider";
 
 import SignIn from "./SignIn";
+import Dashboard from "./Dashboard";
 import OnboardingController from "./OnboardingController";
 import Footer from "./Footer";
+
+import "./App.css";
 
 function App() {
   const user = useContext(UserContext);
   return (
     <div className="App">
       <SignIn></SignIn>
+      {user && <Dashboard />}
       {!user && <OnboardingController />}
       <Footer></Footer>
     </div>
