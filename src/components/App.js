b884f2963e70b787948f089bca350831ee884156
Krista Rutz <rutzkri000@gmail.com>
2020-12-09 10:39:46 -0800

    Reorganize app layout

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,29 @@
 import React, { useContext } from "react";
 import { UserContext } from "../providers/UserProvider";
+import styled from "styled-components";
 
-import SignIn from "./SignIn";
 import Dashboard from "./Dashboard";
 import OnboardingController from "./OnboardingController";
 import Footer from "./Footer";
+import NavBar from "./NavBar";
 
 import "./App.css";
+
+const Body = styled.div`
+  min-height: 100vh;
+  padding: 2vh 5vw 5vh 5vw;
+`;
 
 function App() {
   const user = useContext(UserContext);
   return (
     <div className="App">
-      <SignIn></SignIn>
-      {user && <Dashboard />}
-      {!user && <OnboardingController />}
-      <Footer></Footer>
+      {/* <NavBar /> */}
+      <Body>
+        {user && <Dashboard />}
+        {!user && <OnboardingController />}
+      </Body>
+      <Footer />
     </div>
   );
 }
