c9a6aada78a93c62f6d8068fea187a40b91983d1
Krista Rutz <rutzkri000@gmail.com>
2020-12-09 19:06:01 -0800

    Remove NavBar

--- src/components/App.js
+++ src/components/App.js
@@ -5,12 +5,11 @@ import styled from "styled-components";
 import Dashboard from "./Dashboard";
 import OnboardingController from "./OnboardingController";
 import Footer from "./Footer";
-import NavBar from "./NavBar";
 
 import "./App.css";
 
 const Body = styled.div`
-  min-height: 100vh;
+  min-height: 95vh;
   padding: 2vh 5vw 5vh 5vw;
 `;
 
@@ -18,7 +17,6 @@ function App() {
   const user = useContext(UserContext);
   return (
     <div className="App">
-      {/* <NavBar /> */}
       <Body>
         {user && <Dashboard />}
         {!user && <OnboardingController />}
