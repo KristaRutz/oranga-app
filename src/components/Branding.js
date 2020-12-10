92e01621207f20ab5af184a89860e95aca370fa4
Krista Rutz <rutzkri000@gmail.com>
2020-12-09 19:05:34 -0800

    Assign colors to variable names

---
+++ src/components/Branding.js
@@ -0,0 +1,6 @@
+export const orng = "#FFCC99";
+export const grn = "#A2D3C3";
+export const beige = "#F0E3DF";
+export const pnk = "#FFC0CB";
+export const blk = "#2F3D4C";
+export const bg = "#E5E5E5";
