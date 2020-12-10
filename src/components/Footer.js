f369dba35f2e464e3447ed900953f4b3a60f9ccc
Krista Rutz <rutzkri000@gmail.com>
2020-12-09 19:06:18 -0800

    Update footer styles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,16 @@
 import React from "react";
+import styled from "styled-components";
+
+const P = styled.p`
+  color: #2f3d4c;
+  opacity: 50%;
+  padding: 2vw;
+  font-size: 12px;
+  font-style: italic;
+  min-height: 5vh;
+`;
 
 function Footer() {
-  return <p>Copyright 2020 Sam Wong, Krista Rutz, Becky Zhang</p>;
+  return <P>Copyright 2020 Krista Rutz, Sam Wong, Becky Zhang</P>;
 }
 export default Footer;
